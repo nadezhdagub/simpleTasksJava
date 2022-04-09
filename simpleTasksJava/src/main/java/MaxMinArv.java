bf025cf28bb07c3a413eb51cc1d6c36ef8f5a83e
nadezhdagub <41242817+nadezhdagub@users.noreply.github.com>
2022-04-10 01:23:07 +0300

    Update MaxMinArv.java

--- simpleTasksJava/src/main/java/MaxMinArv.java
+++ simpleTasksJava/src/main/java/MaxMinArv.java
@@ -1,10 +1,12 @@
 public class MaxMinArv {
     public static void main(String[] args) {
+        
         int n = 30;
         double[] arr = new double[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = Math.random();
         }
+        
         double max = arr[0];
         double min = arr[0];
         double agv = 0.0;
@@ -18,6 +20,7 @@ public class MaxMinArv {
             }
             s += arr[i];
         }
+        
         agv = s / arr.length;
         System.out.println(max + min + agv);
     }
