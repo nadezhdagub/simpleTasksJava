4e6e53595370590b222c6b394501b4524ef5be04
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-05-26 23:49:00 +0300

    Add files via upload

---
+++ javaSimpleTasks/src/main/java/ExArrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExArrayList {
+    public static void main(String[] args) {
+        // Write a Java program to create a new list of arrays,
+        // add some elements (strings) and print the collection.
+        List<String> list = new ArrayList<String>();
+        list.add("RED");
+        list.add("GREEN");
+        list.add("Orange");
+        list.add("White");
+        System.out.println(list);
+    }
+}
---
+++ javaSimpleTasks/src/main/java/ExLinketList.java
@@ -0,0 +1,2 @@
+public class ExLinketList {
+}
